89ada85f700eda01554269ef0ddb7525af86aa9a
Jacob Ruleaux <jakeruleaux@gmail.com>
2020-10-11 09:53:48 -0600

    remove loader comp and add kitten images

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import { Grid, Image } from 'semantic-ui-react';
 import pentagram from '../images/pentagram.png';
+import fire from '../images/fire.jpg';
+import record from '../images/record.png';
+
+const backDrop = {
+    height: '100%',
+    width: '100%',
+    backgroundImage: `url(${fire})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundSize: '100% 100%',
+    paddingTop: '100px',
+    paddingBottom: '100px'
+}
+
+const albumContainer = {
+
+}
 
 const albumCover = {
     width: '600px',
@@ -10,38 +27,42 @@ const albumCover = {
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center',
     backgroundColor: '#303030',
-    textAlign: 'center'
+    textAlign: 'center',
+    display: 'table',
+    margin: '0 auto'
 }
 
-const bandTitle ={
+const bandTitle = {
     textShadow: '0 0 10px red'
 }    
 const albumTitle = {
     textShadow: '0 0 10px red',
-    marginTop: '50%'
+    marginTop: '55%'
 }
 
-const pentagramStyle = {
-    width: '500px'
+const recordImage = {
+    height: '600px',
+    width: '290px',
+    backgroundImage: `url(${record})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
 }
+
+
+// const pentagramStyle = {
+//     width: '500px'
+// }
 
 const Splash = () => {
     return(
-        <div>
-            <Grid celled='internally'>
-                <Grid.Row>
-                <Grid.Column width={3}>
-                </Grid.Column>
-                <Grid.Column width={10}>
-                    <div style={albumCover}>
-                        <h1 style={bandTitle}>kittens from Hell</h1>
-                        <h1 style={albumTitle}>the CAT Album</h1>
-                    </div>
-                </Grid.Column>
-                <Grid.Column width={3}>
-                </Grid.Column>
-                </Grid.Row>
-            </Grid>    
+        <div style={backDrop}>
+            <div style={albumContainer}>
+                <div style={albumCover}>
+                    <h1 style={bandTitle}>kittens from Hell</h1>
+                    <h1 style={albumTitle}>the CAT Album</h1>
+                </div>
+                <div style={recordImage} />
+            </div>
         </div>
     )
 }
